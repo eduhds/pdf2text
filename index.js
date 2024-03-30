4468d45a7b1a39641f76c6100b5ae5e73f50b519
Eduardo H. da Silva <27dhiouz9@mozmail.com>
2024-03-30 14:33:07 -0300

    Initial

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S node --no-warnings
+
+const fs = require('fs');
+const pdf = require('pdf-parse');
+
+const args = process.argv.slice(2);
+
+const pdfFile = args[0];
+
+(async () => {
+  try {
+    if (!pdfFile || !pdfFile.endsWith('.pdf')) {
+      throw new Error('Invalid argument. Must pass a PDF file like: "./pdf2text file.pdf"');
+    }
+    const data = await pdf(fs.readFileSync(pdfFile));
+    console.log(data.text);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})();
